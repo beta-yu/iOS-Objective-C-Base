8a6f90c1b37f24ee124a9b96d6ab5b6fa9d0cc67
qiyu <qiyu@p1.com>
2020-03-08 20:35:07 +0800

    Initial Commit

---
+++ LearnBase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnBase
+//
+//  Created by qiyu on 2020/3/8.
+//  Copyright © 2020 com.qiyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
